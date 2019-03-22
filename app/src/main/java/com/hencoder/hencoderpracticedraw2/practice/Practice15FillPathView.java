c0b2ba5bb5eafa7bc98631fec50647cf0c1616e9
Syun0929 <Syun0929@gmail.com>
2019-03-22 19:29:39 +0800

    finish

--- app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice15FillPathView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice15FillPathView.java
@@ -48,7 +48,9 @@ public class Practice15FillPathView extends View {
         paint.setStyle(Paint.Style.FILL_AND_STROKE);
         paint.setStrokeWidth(0);
         // 第一处：获取 Path
+        paint.getFillPath(path,path1);
         canvas.drawPath(path, paint);
+
 
         canvas.save();
         canvas.translate(500, 0);
@@ -59,8 +61,10 @@ public class Practice15FillPathView extends View {
         canvas.translate(0, 200);
         paint.setStyle(Paint.Style.STROKE);
         // 第二处：设置 Style 为 STROKE 后再获取 Path
+        paint.getFillPath(path,path2);
         canvas.drawPath(path, paint);
         canvas.restore();
+
 
         canvas.save();
         canvas.translate(500, 200);
@@ -71,8 +75,10 @@ public class Practice15FillPathView extends View {
         canvas.translate(0, 400);
         paint.setStrokeWidth(40);
         // 第三处：Style 为 STROKE 并且线条宽度为 40 时的 Path
+        paint.getFillPath(path,path3);
         canvas.drawPath(path, paint);
         canvas.restore();
+
 
         canvas.save();
         canvas.translate(500, 400);
